23248ded244c89a678091863c246745b392aeef1
MauricioSJMW <mauricio092297@gmail.com>
2022-09-02 17:17:29 -0500

    jjj

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mapas_mau/provider/map/map_controller.dart';
 import 'package:provider/provider.dart';
@@ -25,6 +26,7 @@ class MapView extends StatelessWidget {
     final providerLocation = Provider.of<MyLocation>(context);
     // ignore: no_leading_underscores_for_local_identifiers
     Completer<GoogleMapController> _controller = Completer();
+    String buscarDireccion;
     const estiloNoche =
         '[{"elementType":"geometry","stylers":[{"color":"#1d2c4d"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#8ec3b9"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#1a3646"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"color":"#4b6878"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#64779e"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"color":"#4b6878"}]},{"featureType":"landscape.man_made","elementType":"geometry.stroke","stylers":[{"color":"#334e87"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#023e58"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#283d6a"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#6f9ba5"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#023e58"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#3C7680"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#304a7d"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#98a5be"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#2c6675"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#255763"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#b0d5ce"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#023e58"}]},{"featureType":"transit","elementType":"labels.text.fill","stylers":[{"color":"#98a5be"}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"color":"#283d6a"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#3a4762"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#0e1626"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#4e6d70"}]}]';
 
@@ -33,28 +35,53 @@ class MapView extends StatelessWidget {
 
     const estiloGris =
         '[{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}]';
+    GoogleMapController mapController;
     final size = MediaQuery.of(context).size;
-    return SizedBox(
-        width: size.width,
-        height: size.height,
-        child: Listener(
-          child: GoogleMap(
-            mapType: MapType.normal,
-            compassEnabled: false,
-            initialCameraPosition: provider.initialCameraPosition,
-            myLocationEnabled: true,
-            myLocationButtonEnabled: true,
-            markers: provider.markers,
-            onTap: provider.onTap,
-            onMapCreated: (GoogleMapController controller) {
-              controller.setMapStyle(estiloDia);
-              _controller.complete(controller);
-            },
-            //onCameraMove: (position) {
-            //providerLocation.position = position.target;
-            //   mapBLoc.mapCenter = position.target;
-            //},
-          ),
-        ));
+    return Scaffold(
+        body: Stack(children: <Widget>[
+      GoogleMap(
+        mapType: MapType.normal,
+        compassEnabled: false,
+        initialCameraPosition: provider.initialCameraPosition,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        markers: provider.markers,
+        onTap: provider.onTap,
+        onMapCreated: (GoogleMapController controller) {
+          controller.setMapStyle(estiloDia);
+          _controller.complete(controller);
+        },
+        //onCameraMove: (position) {
+        //providerLocation.position = position.target;
+        //   mapBLoc.mapCenter = position.target;
+        //},
+      ),
+      Positioned(
+        top: 30.0,
+        right: 15.0,
+        left: 15.0,
+        child: Container(
+          height: 50.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0), color: Colors.white),
+          child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Ingrese Direccion a Buscar',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
+                suffixIcon: IconButton(
+                  icon: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {},
+                    iconSize: 30.0,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              onChanged: (val) {}),
+        ),
+      )
+    ]));
   }
 }
